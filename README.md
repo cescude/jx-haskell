074f25b2520c27bdfe5fda6810c80048927d95eb
Chandler Escude <chandler.escude@gmail.com>
2023-09-10 17:23:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Another implementation of jx in Haskell this time. A couple things:
+
+* Haven't written Haskell in ~15 years, looks like GHC is all that's left
+  anymore?
+* Wanted to see what I can get away with without dipping into packages,
+  bytestrings, and GHC extensions
+
+Interesting that it's implemented like a recursive decent parser, but, due to
+laziness, the runtime behaves more like the char-at-a-time processor I wrote
+for `jx-dlang`. 
+
+Quick comparison of three versions against a 181M test file (dumping stdout to
+/dev/null, to eliminate that aspect):
+
+* this one processes it in 13s
+* dlang version is 7.1s
+* zig version is 6.9s
---
+++ jx.hs
@@ -0,0 +1,106 @@
+import Control.Monad
+import Data.Char
+import Data.List
+
+main = do
+  json <- getContents
+  mapM_ printLine (parse json)
+
+printLine (key, value) = do
+  putStr key
+  putStr "    "
+  putStrLn value
+
+test1 = "   {    \"one\"    :  \"two\"    }"
+test2 = "{\"one\":\"two\"}"
+test3 = "{\"one\" :{ \"two\" : true}, \"four\" : 5}"
+test4 = "{\"one\" :{ \"two\" : true}, \"four\" : 55, \"six\"    :   \"seven!\"}"
+
+test5 = "[1,2,3  ,  4, 5, {\"one\": true, \"two\" :false}]"
+
+data Stack = Top | ObjKey String | ArrIdx Integer deriving Show
+
+parse json = topLevel json
+
+topLevel [] = []
+topLevel (ch:rest)
+  | ch == '{' = objWantKey [Top] rest
+  | ch == '[' = arrWantValue [Top] 0 rest
+  | otherwise = topLevel rest
+
+objWantKey stack (ch:rest)
+  | isSpace ch = objWantKey stack rest
+  | ch == '}' = dropLevel stack rest
+  | ch == '"' = objReadKey stack "" rest
+
+objReadKey stack kacc (ch:rest)
+  | ch == '\\' = objReadKeyEsc stack (ch:kacc) rest
+  | ch == '"' = objWantColon stack (reverse kacc) rest
+  | otherwise = objReadKey stack (ch:kacc) rest
+
+objReadKeyEsc stack kacc (ch:rest) = objReadKey stack (ch:kacc) rest
+
+objWantColon stack key (ch:rest)
+  | isSpace ch = objWantColon stack key rest
+  | ch == ':' = objWantValue stack key rest
+
+objWantValue stack key (ch:rest)
+  | isSpace ch = objWantValue stack key rest
+  | ch == '{' = objWantKey (ObjKey key : stack) rest
+  | ch == '[' = arrWantValue (ObjKey key : stack) 0 rest
+  | ch == '"' = objReadStr stack key [ch] rest
+  | otherwise = objReadBare stack key [ch] rest
+
+objReadStr stack key vacc (ch:rest)
+  | ch == '\\' = objReadStrEsc stack key (ch:vacc) rest
+  | ch == '"' = (mkKey stack key, reverse $ ch:vacc) : objAfterVal stack rest
+  | otherwise = objReadStr stack key (ch:vacc) rest
+
+objReadStrEsc stack key vacc (ch:rest) = objReadStr stack key (ch:vacc) rest
+
+objReadBare stack key vacc (ch:rest)
+  | isSpace ch = (mkKey stack key, reverse vacc) : objAfterVal stack rest
+  | ch == '}' = (mkKey stack key, reverse vacc) : dropLevel stack rest
+  | ch == ',' = (mkKey stack key, reverse vacc) : objWantKey stack rest
+  | otherwise = objReadBare stack key (ch:vacc) rest
+
+objAfterVal stack (ch:rest)
+  | isSpace ch = objAfterVal stack rest
+  | ch == '}' = dropLevel stack rest
+  | ch == ',' = objWantKey stack rest
+
+arrWantValue stack idx (ch:rest)
+  | isSpace ch = arrWantValue stack idx rest
+  | ch == '{' = objWantKey (ArrIdx idx : stack) rest
+  | ch == '[' = arrWantValue (ArrIdx idx : stack) 0 rest
+  | ch == '"' = arrReadStr stack idx [ch] rest
+  | otherwise = arrReadBare stack idx [ch] rest
+
+arrReadStr stack idx vacc (ch:rest)
+  | ch == '\\' = arrReadStrEsc stack idx (ch:vacc) rest
+  | ch == '"' = (mkKey stack $ show idx, reverse $ ch:vacc) : arrAfterVal stack idx rest
+  | otherwise = arrReadStr stack idx (ch:vacc) rest
+
+arrReadStrEsc stack idx vacc (ch:rest) = arrReadStr stack idx (ch:vacc) rest
+
+arrReadBare stack idx vacc (ch:rest)
+  | isSpace ch = (mkKey stack $ show idx, reverse vacc) : arrAfterVal stack idx rest
+  | ch == ']' = (mkKey stack $ show idx, reverse vacc) : dropLevel stack rest
+  | ch == ',' = (mkKey stack $ show idx, reverse vacc) : arrWantValue stack (idx+1) rest
+  | otherwise = arrReadBare stack idx (ch:vacc) rest
+
+arrAfterVal stack idx (ch:rest)
+  | isSpace ch = arrAfterVal stack idx rest
+  | ch == ']' = dropLevel stack rest
+  | ch == ',' = arrWantValue stack (idx+1) rest
+
+-- Pop the top of the stack and enter the state machine at the right spot
+dropLevel (tos:stack) str =
+  case tos of
+    Top -> topLevel str
+    ObjKey k -> objAfterVal stack str
+    ArrIdx i -> arrAfterVal stack i str
+
+mkKey (Top : _) key = key
+mkKey (ObjKey k : stack) key = mkKey stack $ k ++ "." ++ key
+mkKey (ArrIdx i : stack) key = mkKey stack $ (show i) ++ "." ++ key
